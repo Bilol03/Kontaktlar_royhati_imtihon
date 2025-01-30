764ac26249bde5eddd6758d29e575820049b5341
Bilol <madiyarovbilol78@gmail.com>
2025-01-30 11:57:26 +0500

    Kontaktlarni o'chirish qismi tugadi

--- v6.js
+++ v6.js
@@ -1,14 +1,43 @@
 let datas = []
 let reg = /^\+?\d{1,3}[-.\s]?\(?\d{1,4}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
+let ul = document.getElementById('contactList')
 
+
+function createEl(datas) {
+
+    ul.innerHTML = ""
+    for(let data of datas) {
+        let li = document.createElement('li')
+        let name_el = document.createElement('span')
+        let phone_el = document.createElement('span')
+        let email_el = document.createElement('span')
+
+
+        name_el.className = "ism"
+        let img = document.createElement('img')
+        img.className = 'delete'
+        let btn = document.createElement('button')
+        btn.className = 'delete_btn'
+        btn.style.background = '#f9f9f9'
+
+        img.src = './delete.png'
+        name_el.textContent = data.name
+        phone_el.textContent = data.phone
+        email_el.textContent = data.email
+        
+        btn.append(img)
+        li.append(name_el, phone_el, email_el, btn)
+        ul.append(li)
+        
+
+    }
+}
 document.getElementById('contactForm').addEventListener('submit', event => {
     event.preventDefault()
 
     let name = document.getElementById("name").value
     let phone = document.getElementById("phone").value
     let email = document.getElementById("email").value
-    let ul = document.getElementById('contactList')
-    console.log(name, phone, email);
 
     if(name.length < 4 || !reg.test(phone)) return alert("Ism yoki telefon raqam noto'g'ri")
     
@@ -18,24 +47,17 @@ document.getElementById('contactForm').addEventListener('submit', event => {
         email
     }
     datas.push(data)
+    createEl(datas)
 
-    for(let data of datas) {
-        let li = document.createElement('li')
-        let name_el = document.createElement('span')
-        let phone_el = document.createElement('span')
-        let email_el = document.createElement('span')
-        let img = document.createElement('img')
-        img.className = 'delete'
-
-        img.src = './delete.png'
-        name_el.textContent = data.name
-        phone_el.textContent = data.phone
-        email_el.textContent = data.email
-
-        li.append(name_el, phone_el, email_el, img)
-        ul.append(li)
+    document.querySelector('.delete_btn').addEventListener('click', event => {
+        let closest = event.target.closest('li')
+        let name = closest.querySelector('.ism')
+        datas = datas.filter(el => el.name != name.textContent)
+        console.log(datas);
         
-
-    }
+        createEl(datas)
+        
+    })
     
 })
+
